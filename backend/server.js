0d79e55eb2479feb342add96b14ca26814d88c27
vaibhav720 <parikhvaibhav19@gmail.com>
2022-11-21 19:22:25 +0530

    backend basic part 2

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const dotenv =require("dotenv");
 const app =express();
-
+ dotenv.config();
 app.get("/",(req,res)=>{
     res.send("API is running");
 });
 app.get("/api/chat",(req,res)=>{
     res.send("API is running");
 });
-app.listen(3000,console.log("server is working on port 3000"))
+app.get("/api/chat/:id",(req,res)=>{
+    res.send("API is running");
+});
+
+const PORT = process.env.PORT
+app.listen(PORT,console.log(PORT))
