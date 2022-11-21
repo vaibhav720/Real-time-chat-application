5ce70d7f3ef8a6fe9db2f42548d291d16a45897b
vaibhav720 <parikhvaibhav19@gmail.com>
2022-11-21 20:12:27 +0530

    database schema created

---
+++ backend/Models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose= require("mongoose");
+
+const userSchema = mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true
+        },
+        email:{
+            type: String,
+            required: true
+        },
+        password:{
+            type: String,
+            required: true
+        },
+        pic:{
+            type: String,
+            required: true,
+            default:""
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+const User = mongoose.Model("User",userSchema );
+
+module.exports = User;
