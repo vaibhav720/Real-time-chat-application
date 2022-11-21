c5078559eb7ceacd6dd9f91a1f84b95aba556d87
vaibhav720 <parikhvaibhav19@gmail.com>
2022-11-21 20:59:39 +0530

    Basic Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 import ChatPage from './Pages/ChatPage';
 import HomePage from './Pages/HomePage';
 
+
 function App() {
   return (
-    <>
-    <Route path="/" exact component={<HomePage/>}/>
-    <Route path="/chats" component={<ChatPage/>}/>
-    </>
+    <div className='App'>
+    <Route path="/" component={HomePage} exact />
+      <Route path="/chats" component={ChatPage} />
+    </div>
   );
 }
 
--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,10 +1,52 @@
 import React from 'react'
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from '../components/Authentication/Login';
+import SignUp from '../components/Authentication/SignUp';
+
 
 function HomePage() {
   return (
-    <div>
-      
-    </div>
+    <Container  maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" justifyContent="center" textAlign="center">
+          Chat App
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 
