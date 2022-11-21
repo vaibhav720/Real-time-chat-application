f0335409cde34c2a8eb4604261f356df6306b063
vaibhav720 <parikhvaibhav19@gmail.com>
2022-11-21 22:08:15 +0530

    connected with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv =require("dotenv");
+const connectDB = require("./config/db");
 const app =express();
  dotenv.config();
+ connectDB();
 app.get("/",(req,res)=>{
     res.send("API is running");
 });
