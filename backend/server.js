fbd3bf323a8680da4e64640e9f4676ed48265057
vaibhav720 <parikhvaibhav19@gmail.com>
2022-11-23 12:47:05 +0530

    chan

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const dotenv =require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes")
+
+
+
+
 const app =express();
+
  dotenv.config();
  connectDB();
+ app.use(express.json());
+
+
+ app.use("/api/user", userRoutes);
 app.get("/",(req,res)=>{
     res.send("API is running");
 });
